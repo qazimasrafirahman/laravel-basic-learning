12e7ecfb9e08f0d77bc21689ea1b54fd1abff0d3
Siam <baust.cse.200201018@gmail.com>
2024-06-07 22:28:41 +0600

    Different kinds of method learn.

--- routeLearning/routes/web.php
+++ routeLearning/routes/web.php
@@ -12,4 +12,33 @@ Route::get('/post', function () {
 
 Route::get('/about', function () {
     return view('about');
+    });
+    
+Route::get('/post/{id}', function (string $id) {
+    return "<h2> POST id: ". $id ." </h2>";
 });
+
+// Numeric Number (0-10)
+Route::get('/post/{id}', function (string $id) {
+    return "<h2> POST id: ". $id ." </h2>";
+})->whereNumber('id');
+
+// String (a-z)
+Route::get('/post/{name}', function (string $name) {
+    return "<h2> POST Name: ". $name ." </h2>";
+})->whereAlpha('name');
+
+// AlphaNumeric (siam10)
+Route::get('/post/{id}', function (string $id) {
+    return "<h2> POST Name and id: ". $id ." </h2>";
+})->whereAlphaNumeric('id');
+
+// in (Second parameeter value only excepted.)
+Route::get('/post/{id}', function (string $id) {
+    return "<h2> POST video or song will you want. I want: ". $id ." </h2>";
+})->whereIn('id', ['video', 'song', 'music']);
+
+// Regular Expression (^ (caret means) Everything else expect 0-7 ) 
+Route::get('/post/{name}', function (string $name) {
+    return "<h2> Regular Expression:  ". $name ." </h2>";
+})->where('name', "[^0-7]+");
